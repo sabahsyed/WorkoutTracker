438eae7e81edd3563450255084936bb0b6b4ddca
Sabah Syed <sabisyed@gmail.com>
2020-07-02 02:00:37 -0700

    Edited html file rouyes

--- routes/html.js
+++ routes/html.js
@@ -2,17 +2,14 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/",(req,res) => {
-    console.log("Inside index.html");
     res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 
 router.get("/exercise",(req,res) => {
-    console.log("Inside exercise.html");
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 })
 
 router.get("/stats",(req,res) => {
-    console.log("Inside stats.html");
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 })
 
