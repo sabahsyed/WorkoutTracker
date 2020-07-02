f74db01c0cb2050459a2dd6cd97a627260a5bbcd
Sabah Syed <sabisyed@gmail.com>
2020-07-02 00:57:25 -0700

    Edited path in server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use(require("./Develop/routes/api"));
+app.use(require("./Develop/routes/html"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
